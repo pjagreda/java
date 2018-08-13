7ef1d9e183e9adc64b6c358be326cd042ee57166
pjag <pjagreda@outlook.com>
2018-08-13 09:55:29 -0600

    initial commit

---
+++ helloworld/src/main/java/com/maven/spring/test/Helloworld/Person.java
@@ -0,0 +1,7 @@
+package com.maven.spring.test.Helloworld;
+
+public class Person {
+	public void speak() {
+		System.out.println("I'm a person");
+	}
+}
---
+++ helloworld/src/main/java/com/maven/spring/test/Helloworld/app.java
@@ -0,0 +1,22 @@
+package com.maven.spring.test.Helloworld;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class app {
+
+	public static void main(String[] args) {
+		
+		//ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext ("com/maven/spring/test/Helloworld/beans/beans.xml");
+		
+		
+		Person person = (Person)context.getBean("person");
+		person.speak();
+				
+		//((FileSystemXmlApplicationContext)context).close();
+		((ClassPathXmlApplicationContext)context).close();
+	}
+
+}
