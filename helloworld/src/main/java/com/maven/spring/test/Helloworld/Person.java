ba372abaa8247c9a7d7b58f0d2521eb75a0dcdf9
pjag <pjagreda@gmail.com>
2018-08-13 13:14:47 -0600

    Add properties to bean

--- helloworld/src/main/java/com/maven/spring/test/Helloworld/Person.java
+++ helloworld/src/main/java/com/maven/spring/test/Helloworld/Person.java
@@ -6,18 +6,30 @@ public class Person {
 	
 	private String name;
 	
+	private int taxId;
+	
+	
+	public void setTaxId(int taxId) {
+		this.taxId = taxId;
+	}
+
 	public Person(int id, String name) {
 		
 		this.id = id;
 		this.name = name;
 	}
 
-	@Override
-	public String toString() {
-		return "Person [id=" + id + ", name=" + name + "]";
-	}
+//	@Override
+//	public String toString() {
+//		return "Person [id=" + id + ", name=" + name + "]";
+//	}
 
 	public void speak() {
 		System.out.println("I'm a person");
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", taxId=" + taxId + "]";
 	}
 }
